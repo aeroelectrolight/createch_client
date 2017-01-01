4650f68eae69bbdd62059f3eaab482f4a5bfabc7
aeroelectrolight <aeroelectrolight@yahoo.fr>
2017-01-02 00:28:34 +0100

    Ajout de axios à la place de vue resource dans le main.js et ajout de la fonction de supression  dans le fenêtre timesheet

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import '../semantic/dist/semantic.css'
+// import '../semantic/dist/semantic.css'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
 import store from './store/store.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 // import semantic from '../semantic/dist/semantic.js'
 import {router} from './router.js'
 
 // import '../semantic/dist/semantic.min.css'
 
 Vue.use(VueRouter)
-Vue.use(VueResource)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
